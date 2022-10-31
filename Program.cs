5e0beae0c16d126a1018bf885a8837992207d69c
Srikar <srikarmylavarapu@gmail.com>
2022-10-31 07:34:51 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,25 +6,31 @@ AlternatingValues(3);
  static void NumberOfStars(int numStars)
  {
      var i = 1;
+     // while the count of stars per row is less than or equal to the number I want
      while (i <= numStars)
      {
+         // create the string of stars
          string output = "";
          var j = 0;
+      
+         // this is similiar to i*'*' in python
          while (j < i)
          {
              output = output + "*";
              j++;
          }
+         // print it
          Console.WriteLine(output);
          i++;
      }
  }
  
-// Prints triangle of stars (see problem 1)
+// Prints triangle of stars with underscores
 static void UnderscoresAndStars(int numStars)
 {
-
+    // key determination is that we always have 5 characters printed
     var output = "";
+    // create a string of stars thats length numstars
     for (
         int i = 0;
         i < numStars;
@@ -37,6 +43,7 @@ static void UnderscoresAndStars(int numStars)
          i < numStars;
          i++)
     {
+        //change the nth position repeatedly
         output = output.Remove(i, 1).Insert(i, "_");
         Console.WriteLine(output);
     }
@@ -51,6 +58,7 @@ static void AlternatingValues(int numStars)
         var j = 0;
         while (j < i)
         {
+            //this is like the fizz buzz trick
             if (j % 2 == 0)
             {
                 output = output + "*";
